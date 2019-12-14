778ee0aaf857b0a976f1ec6ef91e1a384b406130
skshim <nodelay@gmail.com>
2019-12-14 12:30:51 +0900

    Make test environment

---
+++ tests.spec.js
@@ -0,0 +1,20 @@
+let app, server;
+const request = require('supertest');
+
+describe('GET /user', function() {
+  beforeAll(done => {
+    app = require('./server');
+    server = app.listen(3000, done);
+  });
+
+  afterAll(done => {
+    server.close(done);
+  });
+
+  it('responds with json', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
